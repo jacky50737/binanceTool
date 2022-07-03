42cee0dd92ae4d7e02d05fb46e9b4580d4b11349
Jacky <jacky50737@gmail.com>
2022-07-04 02:15:25 +0800

    V1

---
+++ class/DataBaseTool.php
@@ -0,0 +1,88 @@
+<?php
+
+class DataBaseTool
+{
+    protected string $server;    # MySQL/MariaDB 伺服器
+    protected string $user;      # 使用者帳號
+    protected string $password;  # 使用者密碼
+    protected string $dbname;    # 資料庫名稱
+    protected object $connection;
+
+    /**
+     * @var
+     */
+    private static $instance;
+
+    /**
+     * @return DataBaseTool
+     */
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct()
+    {
+        $configs = include(__DIR__ . '/../config/database.php');
+        $this->server = $configs['server'];
+        $this->user = $configs['user'];
+        $this->password = $configs['password'];
+        $this->dbname = $configs['dbname'];
+
+        # 連接 MySQL/MariaDB 資料庫
+        $this->connection = new mysqli($this->server, $this->user, $this->password, $this->dbname);
+    }
+
+    /**
+     * 驗證遊戲期數是否存在(true存在 false不存在)
+     * @param string $game
+     * @return bool
+     */
+    public function checkGame(string $game): bool
+    {
+        $sqlQuery = "SELECT * FROM DATA WHERE game = " . $game . ";";
+
+        if ($this->connection->query($sqlQuery)) {
+            if (!is_null($this->connection->query($sqlQuery)->fetch_row())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param string $apiKey
+     * @param array $data
+     * @return bool
+     */
+    public function upLoadTreadLog(string $apiKey, array $data): bool
+    {
+        $sqlQuery = "INSERT INTO TREAD_LOG" .
+            "(SYMBOL, ORDER_ID,ORDER_SIDE, ORDER_STATUS, ORDER_PRICE, ORDER_QTY, API_KEY, LOG_STATUS)" .
+            " VALUES ('" .strval($data['symbol']) . "', '" .
+            strval($data['orderId']) . "', '" .
+            strval($data['orderSide']) . "', '" .
+            strval($data['orderStatus']) . "', '" .
+            strval($data['averagePrice']) . "',' " .
+            strval($data['originalQuantity']) . "', '" .
+            strval($apiKey) . "',' " . "NEW"."')";
+
+        for ($i = 0; $i < 5; $i++) {
+            if ($this->connection->query($sqlQuery) == TRUE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function closeDB()
+    {
+        # 釋放資源
+        $this->connection->close();
+    }
+
+}
---
+++ receiveTreadData.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * 開發者 User
+ * 創建於 2022/7/3
+ * 使用   PhpStorm
+ * 專案名稱binanceTool
+ */
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+header('Content-Type: application/json');
+
+if (isset($_GET["API_KEY"])) {
+    $rowData = [];
+    $rowData['symbol'] = $_GET['symbol'];
+    $rowData['orderId'] = $_GET['orderId'];
+    $rowData['orderSide'] = $_GET['orderSide'];
+    $rowData['orderStatus'] = $_GET['orderStatus'];
+    $rowData['averagePrice'] = $_GET['averagePrice'];
+    $rowData['originalQuantity'] = $_GET['originalQuantity'];
+    $db = DataBaseTool::getInstance();
+    try {
+        $db->upLoadTreadLog($_GET["API_KEY"], $rowData);
+//        $response = json_decode(file_get_contents($url_all));
+//        $data = ['price' => (double)$response->price];
+//        $data = $rowData["order"]["symbol"];
+        $data = [
+            'status' => '200',
+            'msg' => '新增完成',
+        ];
+    } catch (Exception $e) {
+        $data = [
+            'status' => '400',
+            'msg' => '發生未知的錯誤',
+            'error' => $e->getMessage()
+        ];
+    }
+} else {
+    $data = [
+        'status' => '200',
+        'msg' => '密碼錯誤',
+    ];
+}
+echo json_encode($data);
