59a75aa53ff6f799be614e17240a691583f453ce
JackyKuo <jacky50737@gmail.com>
2022-07-05 10:48:55 +0800

    Create database.example.php

---
+++ config/database.example.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 開發者 User
+ * 創建於 2022/2/20
+ * 使用   PhpStorm
+ * 專案名稱LotteryGameDataSyncer
+ */
+return array(
+    'server' => 'localhost',                    # MySQL/MariaDB 伺服器
+    'user' => 'root',                # 使用者帳號
+    'password' => '',          # 使用者密碼
+    'dbname' => 'cryptoharvester_binance_tool', # 資料庫名稱
+);
