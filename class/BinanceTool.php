2a03698782df70e4deffde389ab50c26c126124f
JackyKuo <jacky50737@gmail.com>
2022-07-07 17:01:06 +0800

    Create BinanceTool.php

---
+++ class/BinanceTool.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * 開發者 User
+ * 創建於 2022/7/4
+ * 使用   PhpStorm
+ * 專案名稱binanceTool
+ */
+
+class BinanceTool{
+    public function transferStockStatus(string $orderSide, string $positionSide){
+        $orderStatus = "異常";
+        switch ($orderSide){
+            case 'BUY':
+                switch ($positionSide){
+                    case 'LONG':
+                        $orderStatus = '開多';
+                        break;
+                    case 'SHORT':
+                        $orderStatus = '平多';
+                        break;
+                }
+                break;
+            case 'SELL':
+                switch ($positionSide){
+                    case 'LONG':
+                        $orderStatus = '開空';
+                        break;
+                    case 'SHORT':
+                        $orderStatus = '平空';
+                        break;
+                }
+                break;
+        }
+        return $orderStatus;
+    }
+}
