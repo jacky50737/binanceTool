2e0652e5d45dfe58e4bc8bb70aa9501af3a17ea9
JackyKuo <jacky50737@gmail.com>
2022-07-11 14:50:33 +0800

    Test

--- setFeature.php
+++ setFeature.php
@@ -15,30 +15,29 @@ try {
     if (isset($_GET["PASSWORD"]) and $_GET["PASSWORD"] == "幣安小工具GCP") {
         $db = DataBaseTool::getInstance();
         if ($db->checkUserFeature($_GET['API_KEY'], $_GET['FEATURE_NAME'])) {
-            var_dump(77777);
-            if ($db->updateUserFeature($_GET['API_KEY'], $_GET['FEATURE_NAME'], $_GET['STATUS'], $_GET['EXPIRED DAY'])) {
-                $data = [
-                    'status' => '200',
-                    'msg' => '資料更新完成',
-                ];
-            } else {
+//            if ($db->updateUserFeature($_GET['API_KEY'], $_GET['FEATURE_NAME'], $_GET['STATUS'], $_GET['EXPIRED DAY'])) {
+//                $data = [
+//                    'status' => '200',
+//                    'msg' => '資料更新完成',
+//                ];
+//            } else {
                 $data = [
                     'status' => '400',
                     'msg' => '尚未順利更新',
                 ];
-            }
-        } else {
-            if ($db->insertUserFeature($_GET['API_KEY'], $_GET['FEATURE_NAME'], $_GET['STATUS'], $_GET['EXPIRED DAY'])) {
-                $data = [
-                    'status' => '201',
-                    'msg' => '資料新增完成',
-                ];
-            } else {
-                $data = [
-                    'status' => '400',
-                    'msg' => '尚未順利新增',
-                ];
-            }
+//            }
+//        } else {
+//            if ($db->insertUserFeature($_GET['API_KEY'], $_GET['FEATURE_NAME'], $_GET['STATUS'], $_GET['EXPIRED DAY'])) {
+//                $data = [
+//                    'status' => '201',
+//                    'msg' => '資料新增完成',
+//                ];
+//            } else {
+//                $data = [
+//                    'status' => '400',
+//                    'msg' => '尚未順利新增',
+//                ];
+//            }
         }
 
         $data = [
