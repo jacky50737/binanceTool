6815c62e1642b4ab65eec695deac6295c5cfff5b
JackyKuo <jacky50737@gmail.com>
2022-07-12 17:41:55 +0800

    ne

--- class/BinanceTool.php
+++ class/BinanceTool.php
@@ -70,7 +70,7 @@ class BinanceTool
     public function getAccountInfo()
     {
         $response = $this->signedRequest('GET', 'fapi/v2/account');
-        return json_decode($response);
+        return json_encode($response);
     }
 
     private function signature($queryString){
