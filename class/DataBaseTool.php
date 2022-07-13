0c6883e8c809d487d168e864e2c3b1e1b40e4d72
JackyKuo <jacky50737@gmail.com>
2022-07-13 16:26:14 +0800

    VV

--- class/DataBaseTool.php
+++ class/DataBaseTool.php
@@ -288,6 +288,7 @@ class DataBaseTool
 
         if ($this->connection->query($sqlQuery)) {
             $rows = $this->connection->query($sqlQuery)->fetch_all();
+            var_dump($rows);
             if (is_array($rows)) {
                 $data = [];
                 foreach ($rows as $row){
