51c316f1e1d57d7f1346e290c2cd7d3fe839b307
JackyKuo <jacky50737@gmail.com>
2022-07-13 18:03:19 +0800

    開啟未完成

--- job/nodeScaner.php
+++ job/nodeScaner.php
@@ -6,12 +6,14 @@
  * 專案名稱binanceTool
  */
 
-require_once '/home/cryptoharvester/public_html/binanceToolApi/vendor/autoload.php';
+require_once '/home/cryptoharvester/public_html/binanceToolApi/class/autoload.php';
 
+//撈DB中的執行名單
 $db = DataBaseTool::getInstance();
 $onListData = $db->checkUserFeatureStatus("AUTO_ORDER_NOTIFY");
-var_dump($onListData);
+//var_dump($onListData);
 
+//取得本機forever列表並轉換成陣列格式
 $listRows = shell_exec('npx forever list');
 $listRows = explode(" ", $listRows);
 $arrForeverList = [];
@@ -20,6 +22,7 @@ foreach ($listRows as $key => $listRow) {
         $arrForeverList[] = $listRow;
     }
 }
+//需執行中帳號的列表
 $liveAccountList = [];
 foreach ($arrForeverList as $row) {
     $step1 = explode(".", $row);
@@ -27,4 +30,46 @@ foreach ($arrForeverList as $row) {
     $result = ['API_KEY' => $step2[2], 'API_SECRET' => $step2[3]];
     $liveAccountList[] = $result;
 }
-var_dump($liveAccountList);
+
+//取出本機Key為後續判斷使用
+$liveAccountListOnlyKey = [];
+foreach ($liveAccountList as $row) {
+    $liveAccountListOnlyKey[] = $row['API_KEY'];
+}
+
+//var_dump($liveAccountList);
+//關閉已被停用的執行並刪除檔案
+foreach ($liveAccountList as $row){
+    if (!in_array($row['API_KEY'],$onListData)){
+        var_dump('npx forever stop '."examples/ws-userdata-{$row['API_KEY']}-{$row['API_SECRET']}.ts");
+        var_dump('rm -f '."examples/ws-userdata-{$row['API_KEY']}-{$row['API_SECRET']}.ts");
+//        shell_exec('npx forever stop '."examples/ws-userdata-$row[0]-$row[1].ts");
+//        shell_exec('rm -f '."examples/ws-userdata-$row[0]-$row[1].ts");
+    }
+}
+//取得DB列表上需要執行的API_SECRET
+//並把未執行的執行起來
+foreach ($onListData as $row){
+    $userData['API_KEY'] = $row;
+    $userData['API_SECRET'] = "";
+    $userData['LINE_ID'] = $db->getLineToken($row);
+    $arrData = $db->getApiKey($userData['LINE_ID']);
+    foreach ($arrData as $data){
+        if($row == $data['API_KEY']){
+            $userData['API_SECRET'] = $data['API_SECRET'];
+        }
+    }
+    if (!in_array($userData['API_KEY'],$liveAccountListOnlyKey)){
+        var_dump("touch examples/ws-userdata-{$userData['API_KEY']}-{$userData['API_SECRET']}.ts");
+        var_dump("\cp examples/ws-userdata.ts examples/ws-userdata-{$userData['API_KEY']}-{$userData['API_SECRET']}.ts");
+        var_dump("npx forever --minUptime=1000 --spinSleepTime=1000 start -c ts-node examples/ws-userdata-{$userData['API_KEY']}-{$userData['API_SECRET']}.ts");
+//        shell_exec("touch examples/ws-userdata-{$userData['API_KEY']}-{$userData['API_SECRET']}.ts");
+//        shell_exec("\cp examples/ws-userdata.ts examples/ws-userdata-{$userData['API_KEY']}-{$userData['API_SECRET']}.ts");
+//        shell_exec("npx forever --minUptime=1000 --spinSleepTime=1000 start -c ts-node examples/ws-userdata-{$userData['API_KEY']}-{$userData['API_SECRET']}.ts");
+    }
+}
+
+
+
+
+
