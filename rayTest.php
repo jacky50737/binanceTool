8d9c405a25c57d6e984f0449b456fa5b05fbf37e
JackyKuo <jacky50737@gmail.com>
2022-07-21 14:31:22 +0800

    V2

--- rayTest.php
+++ rayTest.php
@@ -12,54 +12,46 @@ require_once __DIR__ . '/class/autoload.php';
 header('Content-Type: application/json; charset=utf-8');
 
 var_dump($_GET);
-if (isset($_GET["PASSWORD"]) and $_GET["PASSWORD"] == "幣安小工具GCP") {
-    switch ($_GET["TYPE"]) {
-        case '資產':
-            $data = [
-                'status' => '200',
-                'msg' => [
-                    '帳戶資產' => 7777,
-                    '錢包餘額' => 9999,
-                    '保證金率' => 8,
-                    '當前浮虧' => 2333,
-                    '可用金額' => 6666,
+if (isset($_GET["PASSWORD"]) and $_GET["PASSWORD"] == "GCP") {
+    $data = match ($_GET["TYPE"]) {
+        '1' => [
+            'status' => '200',
+            'msg' => [
+                '帳戶資產' => 7777,
+                '錢包餘額' => 9999,
+                '保證金率' => 8,
+                '當前浮虧' => 2333,
+                '可用金額' => 6666,
+            ],
+        ],
+        '2' => [
+            'status' => '200',
+            'msg' => [
+                '帳號' => 'abc777gg',
+                '密碼' => 'djqwjiojcioawewjoid',
+                '使用者名稱' => '阿銘',
+                '生日' => '1980-12-32',
+                '點卡餘額' => 8888,
+            ],
+        ],
+        '3' => [
+            'status' => '200',
+            'msg' => [
+                '會員功能' => [
+                    '自動開平倉' => 1,
+                    '倉位自動通知' => 0,
                 ],
-            ];
-            break;
-        case '帳號':
-            $data = [
-                'status' => '200',
-                'msg' => [
-                    '帳號' => 'abc777gg',
-                    '密碼' => 'djqwjiojcioawewjoid',
-                    '使用者名稱' => '阿銘',
-                    '生日' => '1980-12-32',
-                    '點卡餘額' => 8888,
+                '加值功能' => [
+                    '保證金率通知' => 1,
                 ],
-            ];
-            break;
-        case '設定檔':
-            $data = [
-                'status' => '200',
-                'msg' => [
-                    '會員功能' => [
-                        '自動開平倉' => 1,
-                        '倉位自動通知' => 0,
-                    ],
-                    '加值功能' => [
-                        '保證金率通知' => 1,
-                    ],
-                    '過期時間' => '2022-12-31',
-                ],
-            ];
-            break;
-        default:
-            $data = [
-                'status' => '400',
-                'msg' =>'未攜帶參數[TYPE]或值不合法',
-            ];
-            break;
-    }
+                '過期時間' => '2022-12-31',
+            ],
+        ],
+        default => [
+            'status' => '400',
+            'msg' => '未攜帶參數[TYPE]或值不合法',
+        ],
+    };
 
 } else {
     $data = [
