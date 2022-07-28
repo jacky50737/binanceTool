08f7cbef6cb0b84db97bebd44cd646645bc1ca87
JackyKuo <jacky50737@gmail.com>
2022-07-28 14:38:14 +0800

    UP

--- setFeature.php
+++ setFeature.php
@@ -13,31 +13,48 @@ require_once __DIR__ . '/class/autoload.php';
 header('Content-Type: application/json; charset=utf-8');
 
 if (isset($_GET["PASSWORD"]) and $_GET["PASSWORD"] == "幣安小工具GCP") {
-    $db = DataBaseTool::getInstance();
-    if ($db->checkUserFeature($_GET['API_KEY'], $_GET['FEATURE_NAME'])) {
-        if ($db->updateUserFeature($_GET['API_KEY'], $_GET['FEATURE_NAME'], $_GET['STATUS'], $_GET['EXPIRED_DAY'])) {
-            $data = [
-                'status' => '200',
-                'msg' => '資料更新完成',
-            ];
-        } else {
-            $data = [
-                'status' => '400',
-                'msg' => '尚未順利更新',
-            ];
+    if(in_array($_GET['STATUS'],['ENABLE','DISABLE'])){
+        $status_chinese = "無狀態";
+        switch ($_GET['STATUS']){
+            case 'ENABLE':
+                $status_chinese = "開";
+                break;
+            case 'DISABLE':
+                $status_chinese = "關";
+                break;
         }
-    } else {
-        if ($db->insertUserFeature($_GET['API_KEY'], $_GET['FEATURE_NAME'], $_GET['STATUS'], $_GET['EXPIRED_DAY'])) {
-            $data = [
-                'status' => '201',
-                'msg' => '資料新增完成',
-            ];
+
+        $db = DataBaseTool::getInstance();
+        if ($db->checkUserFeature($_GET['API_KEY'], $_GET['FEATURE_NAME'])) {
+            if ($db->updateUserFeature($_GET['API_KEY'], $_GET['FEATURE_NAME'], $_GET['STATUS'], $_GET['EXPIRED_DAY'])) {
+                $data = [
+                    'status' => '200',
+                    'msg' => '資料更新完成，目前狀態為：'.$status_chinese,
+                ];
+            } else {
+                $data = [
+                    'status' => '400',
+                    'msg' => '尚未順利更新，請確認是否有權限，或洽詢管理員。',
+                ];
+            }
         } else {
-            $data = [
-                'status' => '400',
-                'msg' => '尚未順利新增',
-            ];
+            if ($db->insertUserFeature($_GET['API_KEY'], $_GET['FEATURE_NAME'], $_GET['STATUS'], $_GET['EXPIRED_DAY'])) {
+                $data = [
+                    'status' => '201',
+                    'msg' => '資料新增完成，目前狀態為：'.$status_chinese,
+                ];
+            } else {
+                $data = [
+                    'status' => '400',
+                    'msg' => '尚未順利更新，請確認是否有權限，或洽詢管理員。',
+                ];
+            }
         }
+    }else{
+        $data = [
+            'status' => '400',
+            'msg' => '參數錯誤',
+        ];
     }
 } else {
     $data = [
