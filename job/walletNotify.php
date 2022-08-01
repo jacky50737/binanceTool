c79c7f4a2738ebf0343131ebc80f08d45e1aaf54
JackyKuo <jacky50737@gmail.com>
2022-08-01 11:33:19 +0800

    Update walletNotify.php

--- job/walletNotify.php
+++ job/walletNotify.php
@@ -14,21 +14,23 @@ $binanceTool = BinanceTool::getInstance();
 
 try {
     $checkList = $db->checkUserFeatureStatus('AUTO_WALLET_NOTIFY');
-    foreach ($checkList as $row){
+    foreach ($checkList as $row) {
         $key = $row;
         $secret = $db->getApiSecret($key)[0];
         $binanceTool->setApiKey($key);
         $binanceTool->setApiSecret($secret);
         $nickName = $db->getNickName($key);
+        $lineToken = $db->getLineToken($key);
         $data = $binanceTool->getAccountInfo();
-        $msg = "\n帳戶名稱：".$nickName."\n" .
-            "帳戶資產(USDT)：".number_format($data['totalInitialMargin'],2)."\n" .
-            "錢包餘額(USDT)：".number_format($data['totalWalletBalance'],2)."\n" .
-            "可用金額(USDT)：".number_format($data['availableBalance'],2)."\n" .
-            "當前浮虧(USDT)：".number_format($data['totalUnrealizedProfit'],2)."\n" .
+        $msg = "\n帳戶名稱：" . $nickName . "\n" .
+            "帳戶資產(USDT)：" . number_format($data['totalInitialMargin'], 2) . "\n" .
+            "錢包餘額(USDT)：" . number_format($data['totalWalletBalance'], 2) . "\n" .
+            "可用金額(USDT)：" . number_format($data['availableBalance'], 2) . "\n" .
+            "當前浮虧(USDT)：" . number_format($data['totalUnrealizedProfit'], 2) . "\n" .
 //            "累計盈虧(USDT)：".number_format()."\n" .
-            "當前保證金率：".number_format($data['totalMaintMargin']/$data['totalMarginBalance']*100,2)."%";
-        $lineTool->sendToAdmin($msg);
+            "當前保證金率：" . number_format($data['totalMaintMargin'] / $data['totalMarginBalance'] * 100, 2) . "%";
+        $lineTool->setToken($lineToken);
+        $lineTool->doLineNotify($msg);
     }
 
 } catch (Exception $exception) {
