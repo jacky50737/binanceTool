c5f3c9ec16f4b0ff9c7cea384e1a47be48446f83
JackyKuo <jacky50737@gmail.com>
2022-08-05 11:59:01 +0800

    Update getAccountInfo.php

--- getAccountInfo.php
+++ getAccountInfo.php
@@ -18,36 +18,39 @@ $data = [
     'status' => '400',
     'msg' => '初始化',
 ];
+try {
+    if (isset($_GET["API_KEY"])) {
+        $key = $_GET["API_KEY"];
+        $secret = $db->getApiSecret($key)[0];
+        $binanceTool->setApiKey($key);
+        $binanceTool->setApiSecret($secret);
+        $nickName = $db->getNickName($key);
+        $rows = $binanceTool->getAccountInfo();
+        if($rows['totalMarginBalance'] != 0){
+            $msg = "\n帳戶名稱：" . $nickName . "\n" .
+                "帳戶資產(USDT)：" . number_format($rows['totalMarginBalance'], 2) . "\n" .
+                "錢包餘額(USDT)：" . number_format($rows['totalWalletBalance'], 2) . "\n" .
+                "可用金額(USDT)：" . number_format($rows['availableBalance'], 2) . "\n" .
+                "當前浮虧(USDT)：" . number_format($rows['totalUnrealizedProfit'], 2) . "\n" .
+                "當前保證金率：" . number_format($rows['totalMaintMargin'] / $rows['totalMarginBalance'] * 100, 2) . "%";
+        }else{
+            $msg = "\n帳戶名稱：" . $nickName . "\n" .
+                "您的錢包暫無任何資產!";
+        }
+        $data = [
+            'status' => '200',
+            'msg' => $msg,
+        ];
 
-if (isset($_GET["API_KEY"])) {
-    $key = $_GET["API_KEY"];
-    $secret = $db->getApiSecret($key)[0];
-    $binanceTool->setApiKey($key);
-    $binanceTool->setApiSecret($secret);
-    $nickName = $db->getNickName($key);
-    $rows = $binanceTool->getAccountInfo();
-    if($rows['totalMarginBalance'] != 0){
-        $msg = "\n帳戶名稱：" . $nickName . "\n" .
-            "帳戶資產(USDT)：" . number_format($rows['totalMarginBalance'], 2) . "\n" .
-            "錢包餘額(USDT)：" . number_format($rows['totalWalletBalance'], 2) . "\n" .
-            "可用金額(USDT)：" . number_format($rows['availableBalance'], 2) . "\n" .
-            "當前浮虧(USDT)：" . number_format($rows['totalUnrealizedProfit'], 2) . "\n" .
-            "當前保證金率：" . number_format($rows['totalMaintMargin'] / $rows['totalMarginBalance'] * 100, 2) . "%";
-    }else{
-        $msg = "\n帳戶名稱：" . $nickName . "\n" .
-            "您的錢包暫無任何資產!";
+    }else {
+        $data = [
+            'status' => '200',
+            'msg' => '參數錯誤',
+        ];
     }
-    $data = [
-        'status' => '200',
-        'msg' => $msg,
-    ];
 
-}else {
-    $data = [
-        'status' => '200',
-        'msg' => '參數錯誤',
-    ];
+    echo json_encode($data);
+}catch (Exception $exception){
+    echo $exception->getMessage();
 }
-
-echo json_encode($data);
 exit(0);
