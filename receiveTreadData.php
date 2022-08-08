d9827964297a56afa846f566d2cc2d636b7f16cc
JackyKuo <jacky50737@gmail.com>
2022-08-08 09:42:11 +0800

    Update receiveTreadData.php

--- receiveTreadData.php
+++ receiveTreadData.php
@@ -25,9 +25,10 @@ try {
         $postMsgData = "無輸入";
     }
 
-    $lineTool->sendToAdmin(__FILE__."\nPOST輸入：\n".$postMsgData);
+//    $lineTool->sendToAdmin(__FILE__."\nPOST輸入：\n".$postMsgData);
 }catch (Exception $exception){
-    $lineTool->sendToAdmin(__FILE__."\nPOST異常：\n".$exception->getMessage());
+    $postMsgData = "異常->".$exception->getMessage();
+//    $lineTool->sendToAdmin(__FILE__."\nPOST異常：\n".$exception->getMessage());
 }
 
 $data = [
@@ -48,7 +49,7 @@ if (isset($_GET["API_KEY"]) and !empty($_GET["API_KEY"])) {
         if ($notifyArray['code'] == '200') {
             if ($lineTool->doLineNotify($notifyArray['msg'])) {
                 $logStatus = "SEND";
-                $lineTool->sendToAdmin(__FILE__ . "\nAPIKEY：{$_GET["API_KEY"]}\n輸出：\n" . $notifyArray['msg']);
+                $lineTool->sendToAdmin(__FILE__ . "\nAPIKEY：{$_GET["API_KEY"]}\nPOST輸入：.$postMsgData.\n輸出：\n" . $notifyArray['msg']);
                 $db->upLoadTreadLog($_GET["API_KEY"], $notifyArray['data'],$logStatus);
             }
         }
