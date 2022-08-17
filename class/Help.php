cf43d37d3b82c18e06425cc49f942e5ea6aee8f8
JackyKuo <jacky50737@gmail.com>
2022-08-17 13:08:04 +0800

    監看主機狀況

--- class/Help.php
+++ class/Help.php
@@ -58,4 +58,34 @@ class Help
             .substr($charid,20,12);
         return $uuid;
     }
+
+    /**
+     * 取得主機RAM使用狀況
+     * @return string
+     */
+    function get_server_memory_usage()
+    {
+
+        $free = shell_exec('free');
+        $free = (string)trim($free);
+        $free_arr = explode("\n", $free);
+        $mem = explode(" ", $free_arr[1]);
+        $mem = array_filter($mem);
+        $mem = array_merge($mem);
+        $memory_usage = $mem[2]/$mem[1]*100;
+
+        return round($memory_usage,2)."%";
+    }
+
+    /**
+     * 取得主機CPU使用狀況
+     * @return string
+     */
+    function get_server_cpu_usage()
+    {
+
+        $load = sys_getloadavg();
+        return $load[0]."%";
+
+    }
 }
---
+++ job/getUsage.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../class/autoload.php';
+
+$lineTool = LineNotify::getInstance();
+$helpTool = Help::getInstance();
+echo "job開始!\n";
+try {
+    $cpu = $helpTool->get_server_cpu_usage();
+    $ram = $helpTool->get_server_memory_usage();
+    $msg = "\n----目前主機狀況----\nCPU：".$cpu."\nRAM：".$ram;
+    $lineTool->sendToAdmin($msg);
+    echo "job結束\n";
+} catch (Exception $exception) {
+    echo '發生錯誤：' . $exception->getMessage();
+    $lineTool->sendToAdmin($exception->getMessage());
+}
