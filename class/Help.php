332244081f666a332aa4086f03f530e4aee7d241
JackyKuo <jacky50737@gmail.com>
2022-08-19 16:49:28 +0800

    Update Help.php

--- class/Help.php
+++ class/Help.php
@@ -84,8 +84,23 @@ class Help
     function get_server_cpu_usage()
     {
 
-        $load = sys_getloadavg();
-        return $load[0]."%";
+//        $load = sys_getloadavg();
+        $stat1 = file('/proc/stat');
+        sleep(1);
+        $stat2 = file('/proc/stat');
+        $info1 = explode(" ", preg_replace("!cpu +!", "", $stat1[0]));
+        $info2 = explode(" ", preg_replace("!cpu +!", "", $stat2[0]));
+        $dif = array();
+        $dif['user'] = $info2[0] - $info1[0];
+        $dif['nice'] = $info2[1] - $info1[1];
+        $dif['sys'] = $info2[2] - $info1[2];
+        $dif['idle'] = $info2[3] - $info1[3];
+        $total = array_sum($dif);
+        $cpu = array();
+        foreach($dif as $x=>$y) $cpu[$x] = round($y / $total * 100, 1);
+        $load = (100 - $cpu['idle']);
+
+        return $load."%";
 
     }
 }
