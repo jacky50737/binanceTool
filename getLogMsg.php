fa826fd1beda52d2377ff8d8121cc367c13e5cc8
JackyKuo <jacky50737@gmail.com>
2022-08-22 14:52:00 +0800

    7777

--- getLogMsg.php
+++ getLogMsg.php
@@ -16,6 +16,7 @@ if (isset($_GET["PASSWORD"]) and $_GET["PASSWORD"] =="幣安小工具GCP") {
     $help = Help::getInstance();
     $db = DataBaseTool::getInstance();
     $arrLog = $db->getTreadLogByOrderId($_GET['ORDER_ID'],['NEW','PARTIALLY_FILLED','FILLED']);
+    var_dump(77777);
     var_dump($arrLog);
     foreach ($arrLog as &$log){
         $log = $help->reArrayFromKey($log);
