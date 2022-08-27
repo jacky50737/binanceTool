989f31c7aff563a69788ffd949468a1dc9c9f51b
Jacky <jacky50737@gmail.com>
2022-08-28 03:54:07 +0800

    關閉顯示錯誤

--- receiveTreadData.php
+++ receiveTreadData.php
@@ -8,8 +8,8 @@
 
 declare(strict_types=1);
 
-ini_set('display_errors','1');
-error_reporting(E_ALL);
+//ini_set('display_errors','1');
+//error_reporting(E_ALL);
 
 require_once 'class/autoload.php';
 
