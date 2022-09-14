abf660c161d45e1bae95fa0e0f490ced52e879cb
JackyKuo <jacky50737@gmail.com>
2022-09-14 14:54:12 +0800

    Update getAccountStatuts.php

--- getAccountStatuts.php
+++ getAccountStatuts.php
@@ -10,6 +10,10 @@ require_once 'class/autoload.php';
 
 header('Content-Type: application/json');
 
+$db = DataBaseTool::getInstance();
+$lineTool = LineNotify::getInstance();
+$binanceTool = BinanceTool::getInstance();
+
 if (isset($_GET["PASSWORD"]) and $_GET["PASSWORD"] =="幣安小工具GCP") {
     if (isset($_GET["LINE_ID"]) and !empty($_GET["LINE_ID"])) {
         $db = DataBaseTool::getInstance();
