cd2a1fd5fd3e2131ec87b3a44aa8f5ee264709cf
JackyKuo <jacky50737@gmail.com>
2022-09-14 14:53:04 +0800

    Update getAccountStatuts.php

--- getAccountStatuts.php
+++ getAccountStatuts.php
@@ -14,10 +14,36 @@ if (isset($_GET["PASSWORD"]) and $_GET["PASSWORD"] =="幣安小工具GCP") {
     if (isset($_GET["LINE_ID"]) and !empty($_GET["LINE_ID"])) {
         $db = DataBaseTool::getInstance();
         $result = $db->getUserWalletStatus($_GET["LINE_ID"]);
-        var_dump($result);
+        foreach ($result as $row) {
+            $key = $row['key'];
+            $secret = $db->getApiSecret($key)[0];
+            $binanceTool->setApiKey($key);
+            $binanceTool->setApiSecret($secret);
+            $nickName = $db->getNickName($key);
+            $lineToken = $db->getLineToken($key);
+            $data = $binanceTool->getAccountInfo();
+            $capital = $db->getUserCapital($key)[0];
+            if ($data['totalMarginBalance'] != 0) {
+                $msg = "\n帳戶名稱：" . $nickName . "\n" .
+                    "帳戶資產(USDT)：" . number_format($data['totalMarginBalance'], 2) . "\n" .
+                    "錢包餘額(USDT)：" . number_format($data['totalWalletBalance'], 2) . "\n" .
+                    "可用金額(USDT)：" . number_format($data['availableBalance'], 2) . "\n" .
+                    "當前浮虧(USDT)：" . number_format($data['totalUnrealizedProfit'], 2) . "\n" .
+                    "當前保證金率：" . number_format($data['totalMaintMargin'] / $data['totalMarginBalance'] * 100, 2) . "%";
+                if ($capital > 0) {
+                    $msg .= "\n" . "當前獲利率：" . number_format((($data['totalMarginBalance']-$capital) / $capital)*100, 2) . "%";
+                }
+    
+            } else {
+                $msg = "\n帳戶名稱：" . $nickName . "\n" .
+                    "您的錢包暫無任何資產!";
+            }
+            $lineTool->setToken($lineToken);
+            $lineTool->doLineNotify($msg);
+        }
         $data = [
             'status' => '200',
-            // 'msg' => $result,
+            'msg' => '已發送通知到Line Notify!!',
         ];
     }
 } else {
