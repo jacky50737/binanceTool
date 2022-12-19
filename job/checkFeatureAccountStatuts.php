58c87057d64f25d0d17c71bdabdc09a707887c53
JackyKuo <jacky50737@gmail.com>
2022-12-19 10:24:49 +0800

    Update checkFeatureAccountStatuts.php

--- job/checkFeatureAccountStatuts.php
+++ job/checkFeatureAccountStatuts.php
@@ -10,7 +10,7 @@ require_once '../class/autoload.php';
 $db = DataBaseTool::getInstance();
 $lineTool = LineNotify::getInstance();
 $binanceTool = BinanceTool::getInstance();
-echo "job開始!\n";
+// echo "job開始!\n";
 try {
     $checkList = $db->checkUserFeatureStatus();
     var_dump($checkList);
@@ -19,18 +19,18 @@ try {
         $secret = $db->getApiSecret($key)[0];
         $binanceTool->setApiKey($key);
         $binanceTool->setApiSecret($secret);
-        echo "將對帳號：$key 執行檢查....\n";
+//         echo "將對帳號：$key 執行檢查....\n";
         if(!$binanceTool->checkBinanceApi()){
-            echo "發現APIKEY：$key 失效! 將進行關閉功能.....\n";
+//             echo "發現APIKEY：$key 失效! 將進行關閉功能.....\n";
             $lineTool->sendToAdmin("\n發現APIKEY：$key 失效! 將進行關閉功能.....\n");
             $is_Successes = $db->updateUserFeature($key,"關閉全功能",'EXPIRED')?"成功":"失敗";
-            echo "關閉{$is_Successes}!\n";
+//             echo "關閉{$is_Successes}!\n";
             $lineTool->sendToAdmin("\n關閉{$is_Successes}!\n");
         }
-        echo "檢查完畢!\n";
+//         echo "檢查完畢!\n";
     }
-    echo "job結束\n";
+//     echo "job結束\n";
 } catch (Exception $exception) {
-    echo '發生錯誤：' . $exception->getMessage();
+//     echo '發生錯誤：' . $exception->getMessage();
     $lineTool->sendToAdmin($exception->getMessage());
 }
