7c34a76b39fb541a55da819d78965e94f7245986
JackyKuo <jacky50737@gmail.com>
2022-12-26 14:13:17 +0800

    Update DataBaseTool.php

--- class/DataBaseTool.php
+++ class/DataBaseTool.php
@@ -555,8 +555,8 @@ class DataBaseTool
      */
     public function updateUserApiLimit(string $lineId, int $apiLimit, string $expiredDay): bool
     {
-        $sqlQuery = "UPDATE ACCOUNT_LIMIT SET API_LIMIT=".$apiLimit." AND EXPIR_DAY='" . $expiredDay ."'WHERE LINE_ID='" . $lineId . "';";
-
+        $sqlQuery = "UPDATE ACCOUNT_LIMIT SET API_LIMIT='(".$apiLimit.")' AND EXPIR_DAY='" . $expiredDay ."'WHERE LINE_ID='" . $lineId . "';";
+var_dump($sqlQuery);
         for ($i = 0; $i < 5; $i++) {
             if ($this->connection->query($sqlQuery)) {
                 return true;
