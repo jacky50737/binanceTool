d6f7f82752c45df4132ff7a7402e650793118f5c
JackyKuo <jacky50737@gmail.com>
2022-12-26 15:21:45 +0800

    Update DataBaseTool.php

--- class/DataBaseTool.php
+++ class/DataBaseTool.php
@@ -110,7 +110,7 @@ class DataBaseTool
     {
         $now = date('T-m-d H:i:s', strtotime('+8 hours'));
         $sqlQuery = "SELECT LINE_ID,API_LIMIT  FROM ACCOUNT_LIMIT WHERE API_LIMIT > (2) AND EXPIR_DAY <'" . $now . "';";
-
+var_dump($sqlQuery);
         if ($this->connection->query($sqlQuery)) {
             $rows = $this->connection->query($sqlQuery)->fetch_all();
             return $rows;
