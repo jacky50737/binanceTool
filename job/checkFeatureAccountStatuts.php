d90a5bf7c1ea87e5661a479650578ee9adfe6d29
JackyKuo <jacky50737@gmail.com>
2022-12-26 16:21:04 +0800

    Update checkFeatureAccountStatuts.php

--- job/checkFeatureAccountStatuts.php
+++ job/checkFeatureAccountStatuts.php
@@ -13,7 +13,7 @@ $binanceTool = BinanceTool::getInstance();
 echo "job開始!\n";
 try {
     $checkList = $db->checkUserFeatureStatus();
-    var_dump($checkList);
+    // var_dump($checkList);
     foreach ($checkList as $row) {
         $key = $row;
         $secret = $db->getApiSecret($key)[0];
@@ -21,10 +21,10 @@ try {
         $binanceTool->setApiSecret($secret);
 //         echo "將對帳號：$key 執行檢查....\n";
         if(!$binanceTool->checkBinanceApi()){
-//             echo "發現APIKEY：$key 失效! 將進行關閉功能.....\n";
+            echo "發現APIKEY：$key 失效! 將進行關閉功能.....\n";
             $lineTool->sendToAdmin("\n發現APIKEY：$key 失效! 將進行關閉功能.....\n");
             $is_Successes = $db->updateUserFeature($key,"關閉全功能",'EXPIRED')?"成功":"失敗";
-//             echo "關閉{$is_Successes}!\n";
+            echo "關閉{$is_Successes}!\n";
             $lineTool->sendToAdmin("\n關閉{$is_Successes}!\n");
         }
 //         echo "檢查完畢!\n";
@@ -32,15 +32,24 @@ try {
 
     //取得過期列表
     $expirList = $db->getApiLimitExpirList();
-    var_dump($expirList);
+    // var_dump($expirList);
     foreach($expirList as $row){
         //取得串接帳號列表(倒序)
         $userAccountList = $db->checkUserAccusesTokenLlist($row[0],'DESC');
         $countNeedDelete = count($userAccountList) - 2;
-        for($i=0;$i<$countNeedDelete;$i++){
-            $db->deleteUser($userAccountList[$i], $row[0]);
+        if($countNeedDelete > 0){
+            $msg = "發現帳號 UID：$row[0] 超出最大可用串接數! 將進行關閉功能.....\n";
+            echo $msg;
+            $lineTool->sendToAdmin("\n{$msg}");
+            for($i=0;$i<$countNeedDelete;$i++){
+                $db->deleteUser($userAccountList[$i], $row[0]);
+            }
+            $msg ="關閉成功!\n";
+            echo $msg;
+            $db->updateUserApiLimit($row[0], 2, date('Y-m-d H:i:s', strtotime('now')));
+            $lineTool->sendToAdmin("\n$msg");
         }
-        $db->updateUserApiLimit($row[0], 2, date('Y-m-d H:i:s', strtotime('now')));
+        
     }
     echo "job結束\n";
 } catch (Exception $exception) {
