6869149435d6b2af6e08b316da342cf608592d44
Jacky <jacky50737@gmail.com>
2022-12-27 22:51:11 +0800

    BugFix

--- job/checkFeatureAccountStatuts.php
+++ job/checkFeatureAccountStatuts.php
@@ -12,23 +12,23 @@ $lineTool = LineNotify::getInstance();
 $binanceTool = BinanceTool::getInstance();
 echo "job開始!\n";
 try {
-    $checkList = $db->checkUserFeatureStatus();
-    // var_dump($checkList);
-    foreach ($checkList as $row) {
-        $key = $row;
-        $secret = $db->getApiSecret($key)[0];
-        $binanceTool->setApiKey($key);
-        $binanceTool->setApiSecret($secret);
-//         echo "將對帳號：$key 執行檢查....\n";
-        if(!$binanceTool->checkBinanceApi()){
-            echo "發現APIKEY：$key 失效! 將進行關閉功能.....\n";
-            $lineTool->sendToAdmin("\n發現APIKEY：$key 失效! 將進行關閉功能.....\n");
-            $is_Successes = $db->updateUserFeature($key,"關閉全功能",'EXPIRED')?"成功":"失敗";
-            echo "關閉{$is_Successes}!\n";
-            $lineTool->sendToAdmin("\n關閉{$is_Successes}!\n");
+        $checkList = $db->checkUserFeatureStatus();
+        // var_dump($checkList);
+        foreach ($checkList as $row) {
+            $key = $row;
+            $secret = $db->getApiSecret($key)[0];
+            $binanceTool->setApiKey($key);
+            $binanceTool->setApiSecret($secret);
+    //         echo "將對帳號：$key 執行檢查....\n";
+            if(!$binanceTool->checkBinanceApi()){
+                echo "發現APIKEY：$key 失效! 將進行關閉功能.....\n";
+                $lineTool->sendToAdmin("\n發現APIKEY：$key 失效! 將進行關閉功能.....\n");
+                $is_Successes = $db->updateUserFeature($key,"關閉全功能",'EXPIRED')?"成功":"失敗";
+                echo "關閉{$is_Successes}!\n";
+                $lineTool->sendToAdmin("\n關閉{$is_Successes}!\n");
+            }
+    //         echo "檢查完畢!\n";
         }
-//         echo "檢查完畢!\n";
-    }
 
 //    //取得過期列表
 //    $expirList = $db->getApiLimitExpirList();
@@ -52,7 +52,7 @@ try {
 //        $msg ="已重設UID：{$row[0]}為最大數量2!\n";
 //        echo $msg;
 //        $lineTool->sendToAdmin("\n$msg");
-    }
+//    }
     echo "job結束\n";
 } catch (Exception $exception) {
     echo '發生錯誤：' . $exception->getMessage();
